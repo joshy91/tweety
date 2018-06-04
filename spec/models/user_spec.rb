14fd6f34202c1f5078dbf490eddc3d467cb15a87
joshy91 <joshua.davidsonsga@gmail.com>
2018-06-04 17:28:04 -0500

    Add tests for user signup requirements

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe User, type: :model do
  	user = User.new(email: 'jojo@mojo.com', password: 'dsfjdslkfjaslfj', name: 'Mojo Jojo', username: "mojoJojo", bio: "crazy evil genius monkey", location: "Townsville")
  	expect(user).to be_valid
  end
+
+ it 'is invalid without a username' do
+ 	user = User.new(email: 'jojo@mojo.com', password: 'dsfjdslkfjaslfj', name: 'Mojo Jojo', username: nil, bio: "crazy evil genius monkey", location: "Townsville")
+ 	expect(user).not_to be_valid
+end
+
+ it 'is invalid without an email' do
+ 	user = User.new(email: nil, password: 'dsfjdslkfjaslfj', name: 'Mojo Jojo', username: "jdlsfjasl;j", bio: "crazy evil genius monkey", location: "Townsville")
+ 	expect(user).not_to be_valid
+end
+
+it 'is invalid without a password' do
+ 	user = User.new(email: 'jojo@mojo.com', password: nil, name: 'Mojo Jojo', username: "mojoJojo", bio: "crazy evil genius monkey", location: "Townsville")
+ 	expect(user).not_to be_valid
+end
+
 end
