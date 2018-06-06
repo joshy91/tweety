1c3fc948551726e3bf22c5e893b56cd664d920d4
joshy91 <joshua.davidsonsga@gmail.com>
2018-06-06 16:30:08 -0500

    Add Avatar uploader and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_223019) do
+ActiveRecord::Schema.define(version: 2018_06_06_212711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_223019) do
     t.string "username"
     t.text "bio"
     t.string "location"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
