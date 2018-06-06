194f6e449064717204a6066bfcf04056202de255
joshy91 <joshua.davidsonsga@gmail.com>
2018-06-06 16:23:10 -0500

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
- it 'is valid with required attributes' do
- 	user = build(:user)
- 	expect(user).to be_valid
- end
+	let(:user) { build(:user) }
 
- it 'is invalid without a username' do
- 	user.username = nil
- 	expect(user).not_to be_valid
-end
+	it 'has a valid factory' do
+		expect(build(:user)).to be_valid
+	end
 
- it 'is invalid without an email' do
- 	user.email = nil
- 	expect(user).not_to be_valid
-end
+  it 'is valid with required attributes' do
+  	#user = User.new(email: 'jojo@mojo.com', password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
+  	expect(user).to be_valid
+  end
 
-it 'is invalid without a password' do
- 	user.password = nil
- 	expect(user).not_to be_valid
-end
+  it 'is invalid without a username' do
+  	user.username = nil
+  	#user = User.new(email: 'jojo@mojo.com', password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: nil, bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
+	it 'is invalid without an email' do
+  	user.email = nil
+  	#user = User.new(email: nil, password: 'xnsaadlahdh98lkjad', name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
+	it 'is invalid without a password' do
+		user.password = nil
+		#user = User.new(email: 'jojo@jojo.com', password: nil, name: 'Mojo Jojo', username: 'mojojojo', bio: 'crazy evil genius monkey', location: 'Townsville')
+		expect(user).not_to be_valid
+	end
+
+	it 'has two tweets' do
+		expect(user.tweets.length).to eq(2)
+	end
+
+	it 'belongs to User' do
+		expect(Tweet.reflect_on_association(:user).macro).to eq(:belongs_to)
+	end
 
 end
